d0e2dfe811f3c7c14e849ab1a53c82c6e7074383
Rue Chidamba <chidambarue@gmail.com>
2021-06-17 05:40:28 -0700

    Print C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main- Entry point
- * Return - always zero
- */
+* main- Entry point
+* Return - always zero
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+ return (0); /*Return value of main */
 }
