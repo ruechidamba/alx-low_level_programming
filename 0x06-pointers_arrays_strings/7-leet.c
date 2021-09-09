c05e8136264bf95e4575a010b9d32a69dfb78c3e
Rutendo Chidamba <chidambarue@gmail.com>
2021-09-09 11:48:21 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * leet - encodes a string into 1337
- * @c: String
- * Return: string that is encoded */
+* leet - encodes a string into 1337
+* @c: String
+* Return: string that is encoded
+*/
 char *leet(char *c)
 {
 char *cp = c;
